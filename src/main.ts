0d094315c679434a4dcb4dee5a4c05c6790e9599
FawzyAshraf <fawzyashrf98@gmail.com>
2025-05-31 10:28:03 +0300

    Adding level 2

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ const camera = new THREE.PerspectiveCamera(
   1000,
 );
 
-function unionAll(...meshes: THREE.Mesh[]) {
-  const [first, ...rest] = meshes;
+function unionAll(meshes: THREE.Mesh[][]) {
+  const [first, ...rest] = meshes.flat();
   return rest.reduce((mesh, total) => CSG.union(mesh, total), first);
 }
 
@@ -22,37 +22,61 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshNormalMaterial();
 const cube = new THREE.Mesh(geometry, material);
 
-const face1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1 / 3, 1 / 3));
-const face2 = new THREE.Mesh(new THREE.BoxGeometry(1 / 3, 1, 1 / 3));
-const face3 = new THREE.Mesh(new THREE.BoxGeometry(1 / 3, 1 / 3, 1));
+const allLevels = [] as THREE.Mesh[][];
+const maxLevel = 2;
 
-const N1 = [] as THREE.Mesh[];
+for (let level = 1; level <= maxLevel; ++level) {
+  let levelObjects = [] as THREE.Mesh[];
+  const levelExp = 3 ** level;
+  const levelExpLower = 3 ** (level - 1);
+  const epsilon = 1e-6;
 
-for (let i = 0; i < 3; ++i) {
-  const p = [1 / 3, 0, -1 / 3];
-  for (let j = 0; j < 3; ++j) {
-    if (i == j && p[i] == 0) {
-      continue;
+  for (
+    let i = -1 / levelExpLower;
+    i < 1 / levelExpLower + epsilon;
+    i += 1 / levelExpLower
+  ) {
+    for (
+      let j = -1 / levelExpLower;
+      j < 1 / levelExpLower + epsilon;
+      j += 1 / levelExpLower
+    ) {
+      if (i == j && Math.abs(i) <= epsilon && level > 1) {
+        continue;
+      }
+      levelObjects.push(
+        new THREE.Mesh(
+          new THREE.BoxGeometry(1 / levelExp, 1 / levelExp, 1).translate(
+            i,
+            j,
+            0,
+          ),
+        ),
+      );
+      levelObjects.push(
+        new THREE.Mesh(
+          new THREE.BoxGeometry(1 / levelExp, 1, 1 / levelExp).translate(
+            i,
+            0,
+            j,
+          ),
+        ),
+      );
+      levelObjects.push(
+        new THREE.Mesh(
+          new THREE.BoxGeometry(1, 1 / levelExp, 1 / levelExp).translate(
+            0,
+            i,
+            j,
+          ),
+        ),
+      );
     }
-    N1.push(
-      new THREE.Mesh(
-        new THREE.BoxGeometry(1 / 9, 1 / 9, 1).translate(p[i], p[j], 0),
-      ),
-    );
-    N1.push(
-      new THREE.Mesh(
-        new THREE.BoxGeometry(1 / 9, 1, 1 / 9).translate(p[i], 0, p[j]),
-      ),
-    );
-    N1.push(
-      new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1 / 9, 1 / 9).translate(0, p[i], p[j]),
-      ),
-    );
   }
+  allLevels.push(levelObjects);
 }
 
-const res = CSG.subtract(cube, unionAll(face1, face2, face3, ...N1));
+const res = CSG.subtract(cube, unionAll(allLevels));
 
 scene.add(res);
 
